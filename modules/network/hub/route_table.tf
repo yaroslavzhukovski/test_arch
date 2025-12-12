d2584f7964c6515e33f44a9b06387a433baed2f9
Yaraslau Zhukouski <yaroslav.zhukovski@gmail.com>
2025-12-12 17:41:13 +0100

    change route table to firewall ip from workload subnet

--- modules/network/hub/route_table.tf
+++ modules/network/hub/route_table.tf
@@ -6,13 +6,4 @@ resource "azurerm_route_table" "hub_rt" {
   tags = var.tags
 }
 
-resource "azurerm_route" "default_to_firewall" {
-  name                = "default-to-firewall"
-  resource_group_name = var.resource_group_name
-  route_table_name    = azurerm_route_table.hub_rt.name
-  address_prefix      = "0.0.0.0/0"
-  next_hop_type       = "VirtualAppliance"
 
-  # Здесь будет IP Firewall — добавим позже
-  next_hop_in_ip_address = module.firewall.private_ip
-}
