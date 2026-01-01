ab957e54069384997e8a80a35b9d558ec489feb5
Yaraslau Zhukouski <yaroslav.zhukovski@gmail.com>
2026-01-01 18:17:03 +0100

    change route table to firewall ip from workload subnet

--- modules/network/hub/outputs.tf
+++ modules/network/hub/outputs.tf
@@ -6,14 +6,8 @@ output "subnet_ids" {
   value = { for k, s in azurerm_subnet.hub_subnets : k => s.id }
 }
 
-output "route_table_id" {
-  value = azurerm_route_table.hub_rt.id
-}
 
 output "subnet_nsg_ids" {
   value = { for k, v in azurerm_network_security_group.subnet_nsg : k => v.id }
 }
 
-output "route_table_name" {
-  value = azurerm_route_table.hub_rt.name
-}
