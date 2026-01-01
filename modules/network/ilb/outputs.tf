d73168d4d3ee0175e9a475c806c8d4a88cbe2728
Yaraslau Zhukouski <yaroslav.zhukovski@gmail.com>
2026-01-01 20:37:15 +0100

    fixing ilb in main module

--- modules/network/ilb/outputs.tf
+++ modules/network/ilb/outputs.tf
@@ -10,24 +10,3 @@ output "backend_pool_id" {
   value = azurerm_lb_backend_address_pool.this.id
 }
 
-module "spoke_ilb" {
-  source = "./modules/network/ilb"
-
-  name                = "${var.project_name}-${var.environment}-spoke-ilb"
-  location            = azurerm_resource_group.platform.location
-  resource_group_name = azurerm_resource_group.platform.name
-
-  frontend_subnet_id = module.spoke_network.subnet_ids["app_frontend"]
-
-  # оставим динамический IP (как в проде тоже часто ок).
-  frontend_private_ip = null
-
-  lb_port      = 80
-  backend_port = 80
-
-  tags = {
-    Project     = var.project_name
-    Environment = var.environment
-    Owner       = var.owner
-  }
-}
