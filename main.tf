09bd53b0b97f8f13a1472bf7e3b83084fb596b6b
Yaraslau Zhukouski <yaroslav.zhukovski@gmail.com>
2026-01-01 20:31:14 +0100

    fixing ilb in main module

--- main.tf
+++ main.tf
@@ -199,3 +199,24 @@ module "spoke_vmss" {
 
   tags = local.tags
 }
+module "spoke_ilb" {
+  source = "./modules/network/ilb"
+
+  name                = "${var.project_name}-${var.environment}-spoke-ilb"
+  location            = azurerm_resource_group.platform.location
+  resource_group_name = azurerm_resource_group.platform.name
+
+  frontend_subnet_id = module.spoke_network.subnet_ids["app_frontend"]
+
+  # оставим динамический IP (как в проде тоже часто ок).
+  frontend_private_ip = null
+
+  lb_port      = 80
+  backend_port = 80
+
+  tags = {
+    Project     = var.project_name
+    Environment = var.environment
+    Owner       = var.owner
+  }
+}
